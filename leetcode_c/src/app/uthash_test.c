4be526205508a1374601cfca914f364fdbca1b2b
yzr95924 <yzr95924@gmail.com>
2023-01-29 00:19:42 +0800

    add the uthash demo

---
+++ leetcode_c/src/app/uthash_test.c
@@ -0,0 +1,10 @@
+/**
+ * @file uthash_test.c
+ * @author Zuoru YANG (zryang@cse.cuhk.edu.hk)
+ * @brief check: https://troydhanson.github.io/uthash/userguide.html
+ * @version 0.1
+ * @date 2023-01-29
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */

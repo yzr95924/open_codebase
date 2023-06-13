c1590700623938a1627740b496525c45d5fb9f4d
yzr95924 <yzr95924@gmail.com>
2023-06-14 01:25:04 +0800

    update dl str/ptr hash tbl struct

--- common_include/c_my_hash.h
+++ common_include/c_my_hash.h
@@ -40,7 +40,7 @@ typedef struct {
 
 /**
  *********************************************
- ***               for int key
+ ***               for int key hash table
  *********************************************
  */
 
@@ -92,6 +92,132 @@ void Zuoru_FreeIntHashTbl(ZUORU_HASH_TBL_INT_ITEM_H **hashTblPtr)
 {
     ZUORU_HASH_TBL_INT_ITEM_H *curItem;
     ZUORU_HASH_TBL_INT_ITEM_H *tmpItem;
+
+    HASH_ITER(hh, *hashTblPtr, curItem, tmpItem) {
+        HASH_DEL(*hashTblPtr, curItem);
+        free(curItem);
+    }
+    free(hashTblPtr);
+    return;
+}
+
+/**
+ *********************************************
+ ***               for ptr key hash table
+ *********************************************
+ */
+
+ZUORU_HASH_TBL_PTR_ITEM_H** Zuoru_InitPtrHashTbl()
+{
+    ZUORU_HASH_TBL_PTR_ITEM_H **hashTblPtr = (ZUORU_HASH_TBL_PTR_ITEM_H**)calloc(1,
+        sizeof(ZUORU_HASH_TBL_PTR_ITEM_H*));
+    *hashTblPtr = NULL;
+    return hashTblPtr;
+}
+
+void Zuoru_InsertPtrHashTbl(ZUORU_HASH_TBL_PTR_ITEM_H **hashTblPtr, void *key,
+    ZUORU_HASH_DATA_T *inVal)
+{
+    ZUORU_HASH_TBL_PTR_ITEM_H *tmpHashItem = NULL;
+    HASH_FIND_PTR(*hashTblPtr, &key, tmpHashItem);
+    if (tmpHashItem == NULL) {
+        tmpHashItem = (ZUORU_HASH_TBL_PTR_ITEM_H*)calloc(1, sizeof(ZUORU_HASH_TBL_PTR_ITEM_H));
+        tmpHashItem->hashKey = key;
+        HASH_ADD_PTR(*hashTblPtr, hashKey, tmpHashItem);
+    }
+    memcpy(&tmpHashItem->hashVal, inVal, sizeof(ZUORU_HASH_DATA_T));
+    return;
+}
+
+bool Zuoru_FindPtrHashTbl(ZUORU_HASH_TBL_PTR_ITEM_H **hashTblPtr, void *key,
+    ZUORU_HASH_TBL_PTR_ITEM_H **ret)
+{
+    ZUORU_HASH_TBL_PTR_ITEM_H *tmpHashItem = NULL;
+    HASH_FIND_PTR(*hashTblPtr, &key, tmpHashItem);
+    if (tmpHashItem == NULL) {
+        *ret = NULL;
+        return false;
+    }
+
+    *ret = tmpHashItem;
+    return true;
+}
+
+void Zuoru_DelPtrHashTbl(ZUORU_HASH_TBL_PTR_ITEM_H **hashTblPtr,
+    ZUORU_HASH_TBL_PTR_ITEM_H *hashItem)
+{
+    HASH_DEL(*hashTblPtr, hashItem);
+    free(hashItem);
+    return;
+}
+
+void Zuoru_FreePtrHashTbl(ZUORU_HASH_TBL_PTR_ITEM_H **hashTblPtr)
+{
+    ZUORU_HASH_TBL_PTR_ITEM_H *curItem;
+    ZUORU_HASH_TBL_PTR_ITEM_H *tmpItem;
+
+    HASH_ITER(hh, *hashTblPtr, curItem, tmpItem) {
+        HASH_DEL(*hashTblPtr, curItem);
+        free(curItem);
+    }
+    free(hashTblPtr);
+    return;
+}
+
+/**
+ *********************************************
+ ***               for str key hash table
+ *********************************************
+ */
+
+ZUORU_HASH_TBL_STR_ITEM_H** Zuoru_InitStrHashTbl()
+{
+    ZUORU_HASH_TBL_STR_ITEM_H **hashTblPtr = (ZUORU_HASH_TBL_STR_ITEM_H**)calloc(1,
+        sizeof(ZUORU_HASH_TBL_STR_ITEM_H*));
+    *hashTblPtr = NULL;
+    return hashTblPtr;
+}
+
+void Zuoru_InsertStrHashTbl(ZUORU_HASH_TBL_STR_ITEM_H **hashTblPtr, const char *key,
+    ZUORU_HASH_DATA_T *inVal)
+{
+    ZUORU_HASH_TBL_STR_ITEM_H *tmpHashItem = NULL;
+    HASH_FIND_STR(*hashTblPtr, key, tmpHashItem);
+    if (tmpHashItem == NULL) {
+        tmpHashItem = (ZUORU_HASH_TBL_STR_ITEM_H*)calloc(1, sizeof(ZUORU_HASH_TBL_STR_ITEM_H));
+        strcpy(tmpHashItem->hashKey, key);
+        HASH_ADD_STR(*hashTblPtr, hashKey, tmpHashItem);
+    }
+    memcpy(&tmpHashItem->hashVal, inVal, sizeof(ZUORU_HASH_DATA_T));
+    return;
+}
+
+bool Zuoru_FindStrHashTbl(ZUORU_HASH_TBL_STR_ITEM_H **hashTblPtr, const char *key,
+    ZUORU_HASH_TBL_STR_ITEM_H **ret)
+{
+    ZUORU_HASH_TBL_STR_ITEM_H *tmpHashItem = NULL;
+    HASH_FIND_STR(*hashTblPtr, key, tmpHashItem);
+    if (tmpHashItem == NULL) {
+        *ret = NULL;
+        return false;
+    }
+
+    *ret = tmpHashItem;
+    return true;
+}
+
+void Zuoru_DelStrHashTbl(ZUORU_HASH_TBL_STR_ITEM_H **hashTblPtr,
+    ZUORU_HASH_TBL_STR_ITEM_H *hashItem)
+{
+    HASH_DEL(*hashTblPtr, hashItem);
+    free(hashItem);
+    return;
+}
+
+void Zuoru_FreeStrHashTbl(ZUORU_HASH_TBL_STR_ITEM_H **hashTblPtr)
+{
+    ZUORU_HASH_TBL_STR_ITEM_H *curItem;
+    ZUORU_HASH_TBL_STR_ITEM_H *tmpItem;
 
     HASH_ITER(hh, *hashTblPtr, curItem, tmpItem) {
         HASH_DEL(*hashTblPtr, curItem);

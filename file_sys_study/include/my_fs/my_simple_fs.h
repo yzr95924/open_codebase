9d696b6cfb58c5c13cfe53f368f64bdf1de13ba7
yzr95924 <yzr95924@gmail.com>
2023-09-28 01:53:49 +0800

    update write superblock

--- file_sys_study/include/my_fs/my_simple_fs.h
+++ file_sys_study/include/my_fs/my_simple_fs.h
@@ -14,16 +14,33 @@
 
 #include <linux/types.h>
 
-#define MY_SIMPLE_BLOCK_SIZE (1 << 12) // 4KiB
+#define SIMPLE_FS_BLOCK_SIZE (1 << 12) // 4KiB
+#define SIMPLE_FS_MAGIC 0xDEADCELL // source: https://en.wikipedia.org/wiki/Hexspeak
+
+/*
+ * simplefs partition layout
+ * +---------------+
+ * |  superblock   |  1 block
+ * +---------------+
+ * |  inode store  |  sb->inode_store_block_num
+ * +---------------+
+ * | ifree bitmap  |  sb->inode_free_bitmap_block_num
+ * +---------------+
+ * | bfree bitmap  |  sb->block_free_bitmap_block_num
+ * +---------------+
+ * |    data       |
+ * |      blocks   |  rest of the blocks
+ * +---------------+
+ */
 
 typedef struct {
-    uint32_t magic;
+    uint32_t magic; // fs magic
     uint32_t total_block_num;
     uint32_t total_inode_num;
 
-    uint32_t inode_store_block_num;
-    uint32_t inode_free_bitmap_block_num;
-    uint32_t block_free_bitmap_block_num;
+    uint32_t inode_store_block_num; // blocks stored inode
+    uint32_t inode_free_bitmap_block_num; // blocks stored inode free-bitmap
+    uint32_t block_free_bitmap_block_num; // blocks stored block free-bitmap
 
     uint32_t free_inode_num;
     uint32_t free_block_num;
@@ -32,5 +49,21 @@ typedef struct {
     uint8_t *free_block_bitmap;
 } simplefs_sb_info;
 
+typedef struct {
+    uint32_t i_mode; // file mode
+    uint32_t i_uid; // owner id
+    uint32_t i_gid; // group id
+    uint32_t i_size; // size in byte
+    uint32_t i_ctime; // inode change time
+    uint32_t i_atime; // inode access time
+    uint32_t i_mtime; // inode modify time
+    uint32_t i_block; // block count
+    uint32_t i_nlink; // hard link count
+    uint32_t i_extent_block; // block with list of extents for this file
+    uint8_t  i_sym_data[32]; // store symlink content
+} __attribute__((packed)) simplefs_inode;
+
+#define SIMPLE_FS_INODE_PER_BLOCK \
+    (SIMPLE_FS_BLOCK_SIZE / sizeof(simplefs_inode))
 
 #endif
--- file_sys_study/src/app/mkfs_simple.c
+++ file_sys_study/src/app/mkfs_simple.c
@@ -14,6 +14,7 @@
 #include "../../../common_include/c_include.h"
 #include "../../../common_include/define_const.h"
 #include "../../../common_include/my_log.h"
+#include "../../../common_include/my_util.h"
 
 #include "../../include/my_fs/my_simple_fs.h"
 
@@ -21,12 +22,65 @@
 
 typedef struct {
     simplefs_sb_info info;
-    uint8_t pad[MY_SIMPLE_BLOCK_SIZE - sizeof(simplefs_sb_info)];
+    uint8_t pad[SIMPLE_FS_BLOCK_SIZE - sizeof(simplefs_sb_info)];
 } __attribute__((packed)) simplefs_sb;
 
+/**
+ * @brief write superblock to the dev/img
+ *
+ * @param fd file descriptor
+ * @param stat_buf stat buffer
+ * @return simplefs_sb* superblock buffer
+ */
 static simplefs_sb* write_superblock(int fd, struct stat *stat_buf)
 {
-    return NULL;
+    simplefs_sb *sb = calloc(1, sizeof(simplefs_sb));
+    if (!sb) {
+        ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME, "alloc superblock failed\n");
+        return NULL;
+    }
+
+    uint32_t total_block_num = stat_buf->st_size / SIMPLE_FS_BLOCK_SIZE;
+    uint32_t total_inode_num = total_block_num;
+    uint32_t mod = total_inode_num % SIMPLE_FS_INODE_PER_BLOCK;
+    if (mod) {
+        total_inode_num += SIMPLE_FS_INODE_PER_BLOCK - mod;
+    }
+    uint32_t inode_store_block_num = ZUORU_DivCeil(total_inode_num,
+        SIMPLE_FS_INODE_PER_BLOCK);
+    uint32_t inode_free_bitmap_block_num = ZUORU_DivCeil(total_inode_num,
+        SIMPLE_FS_BLOCK_SIZE * 8);
+    uint32_t block_free_bitmap_block_num = ZUORU_DivCeil(total_block_num,
+        SIMPLE_FS_BLOCK_SIZE * 8);
+    uint32_t free_block_num =
+        total_block_num - inode_store_block_num - inode_free_bitmap_block_num -
+            block_free_bitmap_block_num;
+
+    sb->info.magic = SIMPLE_FS_MAGIC;
+    sb->info.total_block_num = total_block_num;
+    sb->info.total_inode_num = total_inode_num;
+    sb->info.inode_store_block_num = inode_store_block_num;
+    sb->info.inode_free_bitmap_block_num = inode_free_bitmap_block_num;
+    sb->info.block_free_bitmap_block_num = block_free_bitmap_block_num;
+    sb->info.free_inode_num = total_inode_num - 1;
+    sb->info.free_block_num = free_block_num - 1;
+
+    int ret = write(fd, sb, sizeof(simplefs_sb));
+    if (ret != sizeof(simplefs_sb)) {
+        free(sb);
+        return NULL;
+    }
+
+    ZUORU_LOGGING(INFO_LOG_LEVEL, MODULE_NAME,
+        "write superblock: size=%ld, magic=%#x, total_block_num=%u, "
+        "total_inode_num=%u, inode_store_block_num=%u, "
+        "inode_free_bitmap_block_num=%u, block_free_bitmap_block_num=%u, "
+        "free_inode_num=%u, free_block_num=%u\n", sizeof(simplefs_sb), sb->info.magic,
+        sb->info.total_block_num, sb->info.total_inode_num, sb->info.inode_store_block_num,
+        sb->info.inode_free_bitmap_block_num, sb->info.block_free_bitmap_block_num,
+        sb->info.free_inode_num, sb->info.free_block_num);
+
+    return sb;
 }
 
 int main(int argc, char *argv[])
@@ -52,7 +106,7 @@ int main(int argc, char *argv[])
         ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
             "get file stat failed, errno(%s)\n", strerror(errno));
         ret = EXIT_FAILURE;
-        goto tag_close_file;
+        goto out_close_file;
     }
 
     // get block device size
@@ -62,27 +116,35 @@ int main(int argc, char *argv[])
         if (ret != RETURN_OK) {
             ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
                 "get dev size failed, errno(%s)\n", strerror(errno));
-            goto tag_close_file;
+            goto out_close_file;
         }
         stat_buf.st_size = blk_size;
     }
     ZUORU_LOGGING(INFO_LOG_LEVEL, MODULE_NAME,
-        "get img/dev(%s) size: %ld\n", argv[0], stat_buf.st_size);
+        "get img/dev (%s) size: %ld, blk size: %ld\n", argv[1], stat_buf.st_size,
+        stat_buf.st_blksize);
 
     // check if image/dev is large enough
-    int64_t min_required_size = 1000 * MY_SIMPLE_BLOCK_SIZE;
+    int64_t min_required_size = 1000 * SIMPLE_FS_BLOCK_SIZE;
     if (stat_buf.st_size < min_required_size) {
         ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
             "img/dev is not large enough(%ld), min(%ld)\n",
             stat_buf.st_size, min_required_size);
         ret = EXIT_FAILURE;
-        goto tag_close_file;
+        goto out_close_file;
     }
 
     // write superblock (block 0)
-    simplefs_sb *sb = write_superblock(fd, &stat_buf);
+    simplefs_sb *sb = NULL;
+    sb = write_superblock(fd, &stat_buf);
+    if (!sb) {
+        ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
+            "write superblock failed\n");
+        ret = EXIT_FAILURE;
+        goto out_close_file;
+    }
 
-tag_close_file:
+out_close_file:
     close(fd);
 
     return ret;

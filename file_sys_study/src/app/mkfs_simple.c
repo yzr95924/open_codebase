1aae7bb5e7679746b23e02f931e98412693a19e7
yzr95924 <yzr95924@gmail.com>
2023-09-30 02:32:16 +0800

    finish mkfs proc

--- file_sys_study/src/app/mkfs_simple.c
+++ file_sys_study/src/app/mkfs_simple.c
@@ -56,14 +56,14 @@ static simplefs_sb* write_superblock(int fd, struct stat *stat_buf)
         total_block_num - inode_store_block_num - inode_free_bitmap_block_num -
             block_free_bitmap_block_num;
 
-    sb->info.magic = SIMPLE_FS_MAGIC;
-    sb->info.total_block_num = total_block_num;
-    sb->info.total_inode_num = total_inode_num;
-    sb->info.inode_store_block_num = inode_store_block_num;
-    sb->info.inode_free_bitmap_block_num = inode_free_bitmap_block_num;
-    sb->info.block_free_bitmap_block_num = block_free_bitmap_block_num;
-    sb->info.free_inode_num = total_inode_num - 1;
-    sb->info.free_block_num = free_block_num - 1;
+    sb->info.magic = htole32(SIMPLE_FS_MAGIC);
+    sb->info.total_block_num = htole32(total_block_num);
+    sb->info.total_inode_num = htole32(total_inode_num);
+    sb->info.inode_store_block_num = htole32(inode_store_block_num);
+    sb->info.inode_free_bitmap_block_num = htole32(inode_free_bitmap_block_num);
+    sb->info.block_free_bitmap_block_num = htole32(block_free_bitmap_block_num);
+    sb->info.free_inode_num = htole32(total_inode_num - 1);
+    sb->info.free_block_num = htole32(free_block_num - 1);
 
     int ret = write(fd, sb, sizeof(simplefs_sb));
     if (ret != sizeof(simplefs_sb)) {
@@ -81,6 +81,176 @@ static simplefs_sb* write_superblock(int fd, struct stat *stat_buf)
         sb->info.free_inode_num, sb->info.free_block_num);
 
     return sb;
+}
+
+/**
+ * @brief write the block to store inodes
+ *
+ * @param fd file descriptor
+ * @param sb simplefs superblock
+ * @return int ret value
+ */
+static int write_inode_store_block(int fd, simplefs_sb *sb)
+{
+    int ret = RETURN_OK;
+    uint8_t *block = (uint8_t*)calloc(1, SIMPLE_FS_BLOCK_SIZE);
+    if (!block) {
+        return RETURN_ERROR;
+    }
+
+    simplefs_inode *inode = (simplefs_inode*)block;
+    // skip the inode 0
+    // glibc: readdir will skip inode 0, vfs also avoids using inode 0;
+    inode += 1;
+
+    uint32_t first_data_block = 1 +
+        le32toh(sb->info.block_free_bitmap_block_num) +
+        le32toh(sb->info.inode_free_bitmap_block_num) +
+        le32toh(sb->info.inode_store_block_num);
+
+    inode->i_mode = htole32(S_IFDIR | S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR |
+        S_IWGRP | S_IXUSR | S_IXGRP | S_IXOTH);
+    inode->i_uid = 0;
+    inode->i_gid = 0;
+    inode->i_size = htole32(SIMPLE_FS_BLOCK_SIZE);
+    inode->i_ctime = inode->i_atime = inode->i_mtime = 0;
+    inode->i_block = htole32(1);
+    inode->i_nlink = htole32(2);
+    inode->i_extent_block = htole32(first_data_block);
+
+    // write inode to file
+    ret = write(fd, block, SIMPLE_FS_BLOCK_SIZE);
+    if (ret != SIMPLE_FS_BLOCK_SIZE) {
+        ret = RETURN_ERROR;
+        goto out;
+    }
+
+    // reset the block to all 0
+    memset(block, 0, SIMPLE_FS_BLOCK_SIZE);
+    for (int idx = 1; idx < le32toh(sb->info.inode_store_block_num); idx++) {
+        ret = write(fd, block, SIMPLE_FS_BLOCK_SIZE);
+        if (ret != SIMPLE_FS_BLOCK_SIZE) {
+            ret = RETURN_ERROR;
+            goto out;
+        }
+    }
+
+    ZUORU_LOGGING(INFO_LOG_LEVEL, MODULE_NAME,
+        "write inode store block: %u, inode size=%ld\n",
+        le32toh(sb->info.inode_store_block_num),
+        sizeof(simplefs_inode));
+    ret = RETURN_OK;
+out:
+    free(block);
+    return ret;
+}
+
+static int write_inode_free_bitmap_block(int fd, simplefs_sb *sb)
+{
+    int ret = RETURN_OK;
+    uint8_t *block = calloc(1, SIMPLE_FS_BLOCK_SIZE);
+    if (!block) {
+        return RETURN_ERROR;
+    }
+
+    uint64_t *inode_free_bitmap = (uint64_t*)block;
+    memset(inode_free_bitmap, 0xff, SIMPLE_FS_BLOCK_SIZE);
+
+    inode_free_bitmap[0] = htole64(0xfffffffffffffffc); // have used two inodes: 0xc: 1100
+    ret = write(fd, inode_free_bitmap, SIMPLE_FS_BLOCK_SIZE);
+    if (ret != SIMPLE_FS_BLOCK_SIZE) {
+        ret = RETURN_ERROR;
+        goto out;
+    }
+
+    inode_free_bitmap[0] = 0xffffffffffffffff;
+    for (int idx = 1; idx < le32toh(sb->info.inode_free_bitmap_block_num); idx++) {
+        ret = write(fd, inode_free_bitmap, SIMPLE_FS_BLOCK_SIZE);
+        if (ret != SIMPLE_FS_BLOCK_SIZE) {
+            ret = RETURN_ERROR;
+            goto out;
+        }
+    }
+    ret = RETURN_OK;
+    ZUORU_LOGGING(INFO_LOG_LEVEL, MODULE_NAME, "write inode free bitmap block: %u\n",
+        le32toh(sb->info.inode_free_bitmap_block_num));
+out:
+    free(block);
+    return ret;
+}
+
+static int write_block_free_bitmap_block(int fd, simplefs_sb *sb)
+{
+    int ret = RETURN_OK;
+    // first block: sb + inode_store_block + inode_free_bitmap_block + block_free_bitmap_block
+    uint32_t used_block_num = le32toh(sb->info.inode_store_block_num) +
+        le32toh(sb->info.inode_free_bitmap_block_num) +
+        le32toh(sb->info.block_free_bitmap_block_num) + 2;
+
+    uint8_t *block = calloc(1, SIMPLE_FS_BLOCK_SIZE);
+    if (!block) {
+        return RETURN_ERROR;
+    }
+
+    uint64_t *block_free_bitmap = (uint64_t*)block;
+    memset(block_free_bitmap, 0xff, SIMPLE_FS_BLOCK_SIZE);
+    uint32_t used_bitmap_cnt = 0;
+    uint32_t write_block_cnt = 0;
+    while (used_block_num) {
+        uint64_t cur_line = 0xffffffffffffffff;
+        for (uint64_t mask = 0x1; mask; mask <<= 1) {
+            cur_line &= ~mask;
+            used_block_num--;
+            if (used_block_num == 0) {
+                break;
+            }
+        }
+        block_free_bitmap[used_bitmap_cnt] = htole64(cur_line);
+        used_bitmap_cnt++;
+        if (used_bitmap_cnt * sizeof(uint64_t) == SIMPLE_FS_BLOCK_SIZE) {
+            ret = write(fd, block_free_bitmap, SIMPLE_FS_BLOCK_SIZE);
+            if (ret != SIMPLE_FS_BLOCK_SIZE) {
+                ret = RETURN_ERROR;
+                goto out;
+            }
+            write_block_cnt++;
+            memset(block_free_bitmap, 0xff, SIMPLE_FS_BLOCK_SIZE);
+            used_bitmap_cnt = 0;
+        }
+    }
+
+    if (used_bitmap_cnt != 0) {
+        ret = write(fd, block_free_bitmap, SIMPLE_FS_BLOCK_SIZE);
+        if (ret != SIMPLE_FS_BLOCK_SIZE) {
+            ret = RETURN_ERROR;
+            goto out;
+        }
+        write_block_cnt++;
+    }
+
+    memset(block_free_bitmap, 0xff, SIMPLE_FS_BLOCK_SIZE);
+    for (int idx = write_block_cnt; idx < le32toh(sb->info.block_free_bitmap_block_num); idx++) {
+        ret = write(fd, block_free_bitmap, SIMPLE_FS_BLOCK_SIZE);
+        if (ret != SIMPLE_FS_BLOCK_SIZE) {
+            ret = RETURN_ERROR;
+            goto out;
+        }
+    }
+
+    ZUORU_LOGGING(INFO_LOG_LEVEL, MODULE_NAME, "init write block free bitmap block: %u\n",
+        write_block_cnt);
+    ZUORU_LOGGING(INFO_LOG_LEVEL, MODULE_NAME, "total write block free bitmap block: %u\n",
+        le32toh(sb->info.block_free_bitmap_block_num));
+    ret = RETURN_OK;
+out:
+    free(block);
+    return ret;
+}
+
+static int write_data_blocks(int fd, simplefs_sb *sb)
+{
+    // TODO: can be implemented in the future
+    return RETURN_OK;
 }
 
 int main(int argc, char *argv[])
@@ -125,7 +295,7 @@ int main(int argc, char *argv[])
         stat_buf.st_blksize);
 
     // check if image/dev is large enough
-    int64_t min_required_size = 1000 * SIMPLE_FS_BLOCK_SIZE;
+    int64_t min_required_size = 100 * SIMPLE_FS_BLOCK_SIZE;
     if (stat_buf.st_size < min_required_size) {
         ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
             "img/dev is not large enough(%ld), min(%ld)\n",
@@ -144,6 +314,47 @@ int main(int argc, char *argv[])
         goto out_close_file;
     }
 
+    // write inode store blocks (from block 1)
+    ret = write_inode_store_block(fd, sb);
+    if (ret) {
+        ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
+            "write inode store block failed\n");
+        ret = EXIT_FAILURE;
+        goto out_free_sb;
+    }
+
+    // write inode free bitmap blocks
+    ret = write_inode_free_bitmap_block(fd, sb);
+    if (ret) {
+        ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
+            "write inode free bitmap block failed\n");
+        ret = EXIT_FAILURE;
+        goto out_free_sb;
+    }
+
+    // write block free bitmap blocks
+    ret = write_block_free_bitmap_block(fd, sb);
+    if (ret) {
+        ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
+            "write block free bitmap block failed\n");
+        ret = EXIT_FAILURE;
+        goto out_free_sb;
+    }
+
+    // write the data block section
+    ret = write_data_blocks(fd, sb);
+    if (ret) {
+        ZUORU_LOGGING(ERROR_LOG_LEVEL, MODULE_NAME,
+            "write data blocks failed\n");
+        ret = EXIT_FAILURE;
+        goto out_free_sb;
+    }
+
+    ZUORU_LOGGING(INFO_LOG_LEVEL, MODULE_NAME, "finish the formatting of "
+        "%s\n", argv[1]);
+
+out_free_sb:
+    free(sb);
 out_close_file:
     close(fd);
 
